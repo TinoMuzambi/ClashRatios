ce3cad1ed72bd80eaba50a70d333e6b2f74c4e4e
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-25 18:19:23 +0200

    Adds some markup.

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,40 @@ type Props = {
     }
   }
 
-export default function Home({data} : Props) {
+export default function Home() {
   return (
-    <h1>Hello {data.name}</h1>
+   <>
+   <header>
+     <form className="form">
+       <input type="text" required placeholder="Enter first player's tag 1️⃣" />
+       <p className="vs">vs</p>
+       <input type="text" required placeholder="Enter second player's tag 2️⃣" />
+      <button type="submit">Compare Ratios!🥊</button>
+     </form>
+   </header>
+   <main>
+     <section className="cards">
+       <div className="card first">
+         <h3 className="title">PlayerTag</h3>
+         <h5 className="info">PlayerName</h5>
+         <h5 className="info">PlayerXP</h5>
+         <h5 className="info">PlayerTrophies</h5>
+         <h1 className="ratio">DonationsRatio</h1>
+         <div className="ratios"><p className="calc">Donations</p>
+         <p className="calc">DonationsReceived</p></div>
+       </div>
+       <div className="card first">
+         <h3 className="title">PlayerTag</h3>
+         <h5 className="info">PlayerName</h5>
+         <h5 className="info">PlayerXP</h5>
+         <h5 className="info">PlayerTrophies</h5>
+         <h1 className="ratio">DonationsRatio</h1>
+         <div className="ratios"><p className="calc">Donations</p>
+         <p className="calc">DonationsReceived</p></div>
+       </div>
+     </section>
+   </main>
+   </>
   )
 }
 
