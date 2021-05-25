777e6520b35c0a1c416f503d30ce043aff98768b
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-25 16:58:21 +0200

    Adds some content.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import { GetStaticProps } from 'next'
 
+type Props = {
+    data: string
+  }
 
-export default function Home({data}) {
+export default function Home({data} : Props) {
   return (
     <h1>Hello {data}</h1>
   )
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       data: "Tino"
