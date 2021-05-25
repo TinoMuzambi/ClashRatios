8ec1cf61d50c69ad69df195e57dfc33cabc54a24
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-25 18:43:47 +0200

    Prettier formatting.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,79 @@
-import { GetStaticProps } from 'next'
-import {useState} from "react"
+import { GetStaticProps } from "next";
+import { useState } from "react";
 
 type Props = {
-    data: {
-      tag: string,
-      name: string,
-      explevel: number,
-      trophies: number,
-      donations: number,
-      donationsReceived: number
-    }
-  }
+	data: {
+		tag: string;
+		name: string;
+		explevel: number;
+		trophies: number;
+		donations: number;
+		donationsReceived: number;
+	};
+};
 
 export default function Home() {
-  const [playerData, setPlayerData] = useState({})
-  const [player2Data, setPlayer2Data] = useState({})
-  const [loading, setLoading] = useState(true)
-  return (
-   <>
-   <header>
-     <form className="form">
-      <div className="inputs">
-      <input type="text" required placeholder="Enter player 1️⃣ tag " />
-       <p className="vs">vs</p>
-       <input type="text" required placeholder="Enter player 2️⃣ tag " />
-      </div>
-      <button type="submit">Compare Ratios!🥊</button>
-     </form>
-   </header>
-   <main>
-     <section className="cards">
-       <div className="card first">
-         <h3 className="title">PlayerTag</h3>
-         <h5 className="info">PlayerName</h5>
-         <h5 className="info">PlayerXP</h5>
-         <h5 className="info">PlayerTrophies</h5>
-         <h1 className="ratio">DonationsRatio</h1>
-         <div className="ratios"><p className="calc">Donations</p>
-         <p className="calc">DonationsReceived</p></div>
-       </div>
-       <div className="card second">
-         <h3 className="title">PlayerTag</h3>
-         <h5 className="info">PlayerName</h5>
-         <h5 className="info">PlayerXP</h5>
-         <h5 className="info">PlayerTrophies</h5>
-         <h1 className="ratio">DonationsRatio</h1>
-         <div className="ratios"><p className="calc">Donations</p>
-         <p className="calc">DonationsReceived</p></div>
-       </div>
-     </section>
-   </main>
-   </>
-  )
+	const [playerData, setPlayerData] = useState({});
+	const [player2Data, setPlayer2Data] = useState({});
+	const [loading, setLoading] = useState(true);
+	return (
+		<>
+			<header>
+				<form className="form">
+					<div className="inputs">
+						<input type="text" required placeholder="Enter player 1️⃣ tag " />
+						<p className="vs">vs</p>
+						<input type="text" required placeholder="Enter player 2️⃣ tag " />
+					</div>
+					<button type="submit">Compare Ratios!🥊</button>
+				</form>
+			</header>
+			<main>
+				<section className="cards">
+					<div className="card first">
+						<h3 className="title">PlayerTag</h3>
+						<h5 className="info">PlayerName</h5>
+						<h5 className="info">PlayerXP</h5>
+						<h5 className="info">PlayerTrophies</h5>
+						<h1 className="ratio">DonationsRatio</h1>
+						<div className="ratios">
+							<p className="calc">Donations</p>
+							<p className="calc">DonationsReceived</p>
+						</div>
+					</div>
+					<div className="card second">
+						<h3 className="title">PlayerTag</h3>
+						<h5 className="info">PlayerName</h5>
+						<h5 className="info">PlayerXP</h5>
+						<h5 className="info">PlayerTrophies</h5>
+						<h1 className="ratio">DonationsRatio</h1>
+						<div className="ratios">
+							<p className="calc">Donations</p>
+							<p className="calc">DonationsReceived</p>
+						</div>
+					</div>
+				</section>
+			</main>
+		</>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  let res
-  let tag = "L98JC2LG";
-		tag = tag.replace("#", "");
-		const URL = `https://clash-ratios.herokuapp.com/api/player/${tag}`;
-		try {
-			const result = await fetch(URL);
-      res = await result.json();
-		} catch (error) {
-			console.error(error);
-			alert("Please try a different tag.");
-		} finally {
-
-		}
-  return {
-    props: {
-      data: res
-    }
-  }
-}
+	let res;
+	let tag = "L98JC2LG";
+	tag = tag.replace("#", "");
+	const URL = `https://clash-ratios.herokuapp.com/api/player/${tag}`;
+	try {
+		const result = await fetch(URL);
+		res = await result.json();
+	} catch (error) {
+		console.error(error);
+		alert("Please try a different tag.");
+	} finally {
+	}
+	return {
+		props: {
+			data: res,
+		},
+	};
+};
